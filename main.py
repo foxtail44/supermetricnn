99112e4ee0b680d650e6317020a680507fa6b7eb
Alex Polichroniadis <alexis3000@gmail.com>
2017-06-19 21:21:59 -0400

    added train and val

--- main.py
+++ main.py
@@ -4,20 +4,69 @@ from torch.autograd import Variable
 import model
 import dataloader
 import time
+import os
+import shutil
 
-path = "./dataset/"
+path_train = "./dataset/train/"
+path_validate = "./dataset/validate/"
+
 lr = 0.1
 bs = 10
+epochs = 1000000
 
-trainset = dataloader.MetricDataLoader(path, 0.25)
+resume = None
+start_epoch = 0
+
+trainset = dataloader.MetricDataLoader(path_train, 0.25)
+valset = dataloader.MetricDataLoader(path_trai, 0.25)
+
 
 train_loader = torch.utils.data.DataLoader(trainset, batch_size=bs)
-print trainset[0]
+val_loader = torch.utils.data.DataLoader(valset, batch_size=bs)
+
+
 model = model.CNNSimple(10, 20, 3)
 
 optimizer = torch.optim.SGD(model.parameters(), lr)
 
 criterion = nn.CrossEntropyLoss()
+
+
+def main():
+    # Add code to resume from checkpoint here
+
+    best_prec1 = 0
+
+    # Load checkpoint if flag up
+    if resume:
+        print("**Using pretrained model**")
+        if os.path.isfile(resume):
+            checkpoint = torch.load(resume)
+            start_epoch = checkpoint['epoch']
+            best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['state_dict'])
+            print("**Loaded checkpoint**")
+        else:
+            print("**No checkpoint found**")
+
+    for epoch in range(start_epoch, epochs):
+            adjust_learning_rate(lr, optimizer, epoch)
+            # train for one epoch
+            train(train_loader, model, criterion, optimizer, epoch)
+
+            # evaluate on validation set
+            prec1 = validate(val_loader, model, criterion)
+
+            # remember best prec@1 and save checkpoint
+            is_best = prec1 > best_prec1
+            best_prec1 = max(prec1, best_prec1)
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'arch': 'CNN1',
+                'state_dict': model.state_dict(),
+                'best_prec1': best_prec1,
+            }, is_best)
+
 
 def train(train_loader, model, criterion, optimizer, epoch):
     batch_time = AverageMeter()
@@ -63,7 +112,45 @@ def train(train_loader, model, criterion, optimizer, epoch):
                    epoch, i, len(train_loader), batch_time=batch_time,
                    loss=losses, top1=top1, top5=top5))
 
-train(train_loader, model, criterion, optimizer, 0)
+
+def validate(val_loader, model, criterion, optimizer, epoch):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    end = time.time()
+    for i, (input, target) in enumerate(train_loader):
+        model.train()
+
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        # Set things up
+        input_var = Variable(input)
+        target_var = Variable(target)
+
+        output = model(input_var)
+
+        loss = \
+            criterion(output, target_var)
+
+        prec1, prec5 = accuracy(output.data.cpu(), target_var.data.cpu(), topk=(1, 5))
+        losses.update(loss.data[0], input_var.data.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % 10:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'BTime {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   epoch, i, len(train_loader), batch_time=batch_time,
+                   loss=losses, top1=top1, top5=top5))
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -96,3 +183,19 @@ def accuracy(output, target, topk=(1,)):
         correct_k = correct[:k].view(-1).float().sum(0)
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
+
+
+def adjust_learning_rate(lr, optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = lr * (0.1 ** (epoch // 10))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+if __name__ == '__main__':
+    main()
